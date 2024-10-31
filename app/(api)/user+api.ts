e23f8895e705c97b2b0a6e581c70d492088452cf
Ahmed Ezzat <ah.ezzat.muh@gmail.com>
2024-11-01 01:11:22 +0200

    Hashing password + fixes

--- app/(api)/user+api.ts
+++ app/(api)/user+api.ts
@@ -1,22 +1,50 @@
 import { neon } from "@neondatabase/serverless";
-import bcrypt from "bcrypt";
+
+const generateSalt = (): string => {
+	return Math.random().toString(36).substring(2, 10);
+};
+
+const hashPassword = async (password: string): Promise<string> => {
+	const encoder = new TextEncoder();
+	const data = encoder.encode(password);
+
+	const hashBuffer = await crypto.subtle.digest("SHA-256", data);
+
+	const hashArray = Array.from(new Uint8Array(hashBuffer));
+	const hashHex = hashArray
+		.map((byte) => byte.toString(16).padStart(2, "0"))
+		.join("");
+
+	return hashHex;
+};
 
 export async function POST(request: Request) {
 	try {
 		const sql = neon(`${process.env.DATABASE_URL}`);
-		let { name, email, clerkId, password } = await request.json();
+		const { name, email, clerkId, password } = await request.json();
 		if (!name || !email || !clerkId || !password) {
 			return Response.json(
 				{ error: "Missing required fields" },
 				{ status: 400 }
 			);
 		}
+		const salt = generateSalt();
+		const saltedPassword = `${password}${salt}`;
+		const hashedPassword = await hashPassword(saltedPassword);
 
-		password = await bcrypt.hash(password, 10);
-
-		const query = `INSERT INTO users (name, email, clerk_id, password)
-		VALUES (${name}, ${email}, ${clerkId}, ${password})`;
-		const response = await sql`${query}`;
+		const response = await sql`
+		INSERT INTO users (
+			name, 
+			email, 
+			clerk_id,
+			password
+		  ) 
+		  VALUES (
+			${name}, 
+			${email},
+			${clerkId},
+			${hashedPassword}
+		 );`;
 
 		return new Response(JSON.stringify({ data: response }), {
 			status: 200,
