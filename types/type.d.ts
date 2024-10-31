c2f20cb43bd8252e82baab4d58fb3bc296901f50
Ahmed Ezzat <ah.ezzat.muh@gmail.com>
2024-11-01 01:42:54 +0200

    Integrated geoapify and implemented a drive card ui

--- types/type.d.ts
+++ types/type.d.ts
@@ -1,4 +1,23 @@
 import { TextInputProps, TouchableOpacityProps } from "react-native";
+declare interface Ride {
+	origin_address: string;
+	destination_address: string;
+	origin_latitude: number;
+	origin_longitude: number;
+	destination_latitude: number;
+	destination_longitude: number;
+	ride_time: number;
+	fare_price: number;
+	payment_status: string;
+	driver_id: number;
+	user_id: string;
+	created_at: string;
+	driver: {
+		first_name: string;
+		last_name: string;
+		car_seats: number;
+	};
+}
 
 declare interface ButtonProps extends TouchableOpacityProps {
 	title: string;
