a8ffa54479706da91dd00a7c2d269bcc70785031
Ahmed Ezzat <ah.ezzat.muh@gmail.com>
2024-10-31 21:55:55 +0300

    layout and general setup

--- types/type.d.ts
+++ types/type.d.ts
@@ -1,3 +1,13 @@
+import { TextInputProps, TouchableOpacityProps } from "react-native";
+
+declare interface ButtonProps extends TouchableOpacityProps {
+	title: string;
+	bgVariant?: "primary" | "secondary" | "danger" | "outline" | "success";
+	textVariant?: "primary" | "default" | "secondary" | "danger" | "success";
+	IconLeft?: React.ComponentType<any>;
+	IconRight?: React.ComponentType<any>;
+	className?: string;
+}
 declare interface InputFieldProps extends TextInputProps {
 	label: string;
 	icon?: any;
