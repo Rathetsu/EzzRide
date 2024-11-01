4546affd4246560960e42e15c18f265d9740b498
Ahmed Ezzat <ah.ezzat.muh@gmail.com>
2024-11-01 02:57:22 +0200

    location permission

--- app/(root)/(tabs)/home.tsx
+++ app/(root)/(tabs)/home.tsx
@@ -1,4 +1,6 @@
 import { useUser } from "@clerk/clerk-expo";
+import * as Location from "expo-location";
+import { useEffect, useState } from "react";
 import {
 	ActivityIndicator,
 	FlatList,
@@ -11,13 +13,45 @@ import { SafeAreaView } from "react-native-safe-area-context";
 
 import RideCard from "@/components/RideCard";
 import { icons, images } from "@/constants";
+import { useLoationStore } from "@/store";
 
 
 const Home = () => {
+	const { setUserLocation, setDestinationLocation } = useLoationStore();
 	const { user } = useUser();
 	const loading = false;
 
+	const [hasLocationPermission, setHasLocationPermission] = useState(false);
+
 	const handleSignout = async () => {};
+
+	const handleDesitinationPress = () => {};
+
+	useEffect(() => {
+		const requestLocationPermission = async () => {
+			let { status } = await Location.requestForegroundPermissionsAsync();
+
+			if (status !== "granted") {
+				setHasLocationPermission(false);
+				return;
+			}
+
+			let location = await Location.getCurrentPositionAsync();
+
+			const address = await Location.reverseGeocodeAsync({
+				latitude: location.coords?.latitude!,
+				longitude: location.coords?.longitude!,
+			});
+
+			setUserLocation({
+				latitude: location.coords?.latitude!,
+				longitude: location.coords?.longitude!,
+				address: `${address[0].name}, ${address[0].region}`,
+			});
+		};
+
+		requestLocationPermission();
+	}, []);
 
 	return (
 		<SafeAreaView className="bg-general-500">
